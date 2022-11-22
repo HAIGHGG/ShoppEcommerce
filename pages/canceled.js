5b2e92f36f976ea013d758985aad231472722a9c
Piotr <korotko.piotr@gmail.com>
2022-11-22 16:22:05 +0100

    fixed button

--- pages/canceled.js
+++ pages/canceled.js
@@ -2,14 +2,16 @@ import Link from 'next/link'
 
 const Canceled = () => {
 	return (
-		<div className='flex flex-col h-full text-center' >
+		<div className='flex flex-col h-full text-center'>
 			<h2 className='font-bold text-xl my-5'>Something went wrong with Your Payment</h2>
 			<p className=''>We are unable to process the charges on your payment.</p>
 			<p>We will take you back to home page.</p>
 			<Link href='/'>
-				<button type='button' className='my-5 py-1 px-4 rounded-3xl text-slate-50 bg-indigo-700' width='300px'>
-					Home Page
-				</button>
+				<div>
+					<button type='button' className='my-5 py-1 px-4 rounded-3xl text-slate-50 bg-indigo-700'>
+						Home Page
+					</button>
+				</div>
 			</Link>
 		</div>
 	)
